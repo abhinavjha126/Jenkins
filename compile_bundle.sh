ee689b2cc593505723c5fda68dd6bf8ddaa31658
Kabali <abhinavjha126@gmail.com>
2023-12-21 15:31:21 +0530

    Update compile_bundle.sh

--- compile_bundle.sh
+++ compile_bundle.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 
 VER_TORCH=2.2.0.dev20231010+cpu
 VER_TORCHVISION=0.17.0.dev20231010+cpu
-VER_TORCHAUDIO=2.2.0.dev20231006+cpu
+VER_TORCHAUDIO=2.2.0.dev20231010+cpu
 VER_LLVM=llvmorg-16.0.6
 VER_IPEX=llm_feature_branch
 VER_GCC=12.3.0
